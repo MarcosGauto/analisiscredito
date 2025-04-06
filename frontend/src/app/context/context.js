949afae200f65e6bfc6217ec24677d0d7e62163c
Chipi87 <mgauto@outlook.es>
2025-04-06 20:49:20 -0300

    IVA

---
+++ frontend/src/app/context/context.js
@@ -0,0 +1,11 @@
+const { createContext } = require("react");
+
+
+export const Context = createContext();
+
+export const ContextProvider = () =>{
+
+    const balanceFiscal = debitFiscal - CreditFiscal;
+    const averageIva = ((debitFiscal*0.105)+(debitFiscal*0.21))/2;
+    
+}
